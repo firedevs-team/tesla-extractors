3ca244a2f427db26412ec4310f9f0ebb2e906b5f
kmilo8346 <kmilo8346@gmail.com>
2024-09-03 11:46:52 -0400

    feature(accumulators): added cnev xpeng deliveries

--- apps/sales-accumulator/src/accumulators/index.ts
+++ apps/sales-accumulator/src/accumulators/index.ts
@@ -2,10 +2,12 @@ import { KBAAccumulator } from './KBA';
 import { CNEVTeslaTotalAccumulator } from './CNEVTeslaTotal';
 import { CNEVTeslaRetailAccumulator } from './CNEVTeslaRetail';
 import { CNEVTeslaExportAccumulator } from './CNEVTeslaExport';
+import { CNEVXpengDeliveriesAccumulator } from './CNEVXpengDeliveries';
 
 export default [
   new KBAAccumulator(),
   new CNEVTeslaTotalAccumulator(),
   new CNEVTeslaRetailAccumulator(),
   new CNEVTeslaExportAccumulator(),
+  new CNEVXpengDeliveriesAccumulator(),
 ];
