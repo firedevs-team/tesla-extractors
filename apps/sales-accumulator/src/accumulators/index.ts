ffe6633cd52d3a9f78904522f0f30f39bf2f7aef
kmilo8346 <kmilo8346@gmail.com>
2024-09-03 11:33:39 -0400

    feature(accumulator): addev cnev tesla export

--- apps/sales-accumulator/src/accumulators/index.ts
+++ apps/sales-accumulator/src/accumulators/index.ts
@@ -1,9 +1,11 @@
 import { KBAAccumulator } from './KBA';
 import { CNEVTeslaTotalAccumulator } from './CNEVTeslaTotal';
 import { CNEVTeslaRetailAccumulator } from './CNEVTeslaRetail';
+import { CNEVTeslaExportAccumulator } from './CNEVTeslaExport';
 
 export default [
   new KBAAccumulator(),
   new CNEVTeslaTotalAccumulator(),
   new CNEVTeslaRetailAccumulator(),
+  new CNEVTeslaExportAccumulator(),
 ];
