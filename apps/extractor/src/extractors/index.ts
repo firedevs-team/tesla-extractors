0f8d955d7ca1d1d9196f0f1363f26355385162a3
kmilo8346 <kmilo8346@gmail.com>
2024-09-24 09:09:48 -0300

    feature(yoda): added france logic

--- apps/extractor/src/extractors/index.ts
+++ apps/extractor/src/extractors/index.ts
@@ -1,6 +1,7 @@
 import { BaseExtractor } from '../lib/BaseExtractor';
 import { kbaExtractor } from './germany/KBAExtractor';
+import { pfaExtractor } from './france/PFAExtractor';
 
-const extractors: BaseExtractor[] = [kbaExtractor];
+const extractors: BaseExtractor[] = [kbaExtractor, pfaExtractor];
 
 export default extractors;
